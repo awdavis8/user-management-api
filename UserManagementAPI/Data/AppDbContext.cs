20c7a50450ece3f05821b18792549aaec8fd5ab4
Aaron Davis <awdavis8@ncsu.edu>
2026-03-18 15:00:31 -0400

    Setup database and user model.

--- UserManagementAPI/Data/AppDbContext.cs
+++ UserManagementAPI/Data/AppDbContext.cs
@@ -1,6 +1,23 @@
-﻿namespace UserManagementAPI.Data
+﻿using Microsoft.EntityFrameworkCore;
+using UserManagementAPI.Models;
+
+namespace UserManagementAPI.Data
 {
-    public class AppDbContext
+    public class AppDbContext : DbContext
     {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+        }
+
+        public DbSet<User> Users => Set<User>();
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            modelBuilder.Entity<User>()
+                .HasIndex(u => u.Email)
+                .IsUnique();
+        }
     }
 }
